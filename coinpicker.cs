fe57093023d4d776ad464dd50b055ba312dea82e
Naufal Rafi Akbar <58022016+NaufalRafi-hub@users.noreply.github.com>
2020-06-02 20:07:32 +0700

    untuk men-collect coins

---
+++ coinpicker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class coinpicker : MonoBehaviour
+{
+    private float coin=0;
+
+    public TextMeshProUGUI textcoin;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.transform.tag == "coin")
+        {
+            coin++;
+            textcoin.text = coin.ToString();
+            Destroy(other.gameObject);
+        }
+    }
+}
