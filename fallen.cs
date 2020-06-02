fb4fd7d32930a317c1fa4e67ee8e12c62caff044
Naufal Rafi Akbar <58022016+NaufalRafi-hub@users.noreply.github.com>
2020-06-02 20:12:22 +0700

    jika player jatoh ke air

---
+++ fallen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class fallen : MonoBehaviour
+{
+    private float coin = 0;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.tag == "laot")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        }
+    }
+}
